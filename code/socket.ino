494157b3f1a5aaa2630c68d4d8fd9649232672fc
ngonhu159 <ngonhu159@gmail.com>
2019-04-25 22:55:11 +0700

    update code socket arduino

--- code/socket.ino
+++ code/socket.ino
@@ -8,7 +8,7 @@
 
 SocketIoClient webSocket;
 
-const char* Host_Socket = "ngoquocnhu.herokuapp.com";
+const char* Host_Socket = "ngonhu.herokuapp.com";
 
 //const char* Host_Socket = "192.168.137.1";
 //unsigned int Port_Socket = 3000;
@@ -20,38 +20,40 @@ const char* pwdWifi = "29081999";
 #define ON 1
 #define OFF 0
 
+#define Button_yellow D3 
+#define Button_yellow D4 
 /*==========================================================================================*/
-byte ledStatus = OFF;
-int Val_eeprom;
-char temp = 1;                              
+byte ledStatus = OFF;     
+int Val_eeprom=0;  
+int temp = 0;                     
 /*============================================ Event Socket IO ==============================================*/
 
-void handleMess(const char * payload, size_t length) {
-  DynamicJsonBuffer jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(payload);
-  String from = root["from"];
-  String mess = root["message"];
-  Serial.println(from);
-  Serial.println(mess);
-}
+//void handleMess(const char * payload, size_t length) {
+//  DynamicJsonBuffer jsonBuffer;
+//  JsonObject& root = jsonBuffer.parseObject(payload);
+//  String from = root["from"];
+//  String mess = root["message"];
+//  Serial.println(from);
+//  Serial.println(mess);
+//}
 
-void joinSuccess(const char * payload, size_t length) {
-  webSocket.emit("esp-send-server-update-led","\"updated\"");
-}
+////void joinSuccess(const char * payload, size_t length) {
+//  webSocket.emit("esp-send-server-update-led","\"updated\"");
+//}
 
-void togleLed(const char * payload, size_t length) {
-  ledStatus = !ledStatus;
-}
+//void togleLed(const char * payload, size_t length) {
+//  ledStatus = !ledStatus;
+//}
 
-void yeuCauCapNhat(const char * payload, size_t length) {
+void Web_update_led(const char * payload, size_t length) {
   if (ledStatus) {
-    webSocket.emit("esp-cap-nhat-nhat-trang-thai", "\"ON\"");
+    webSocket.emit("Esp-update-led-to-web", "\"ON\"");
   } else {
-    webSocket.emit("esp-cap-nhat-nhat-trang-thai", "\"OFF\"");
+    webSocket.emit("Esp-update-led-to-web", "\"OFF\"");
   }
 }
 
-void capNhatLed(const char * payload, size_t length) {
+void Turn_on_off_led(const char * payload, size_t length) {
   Serial.print("PayLoad: ");
   for (int i=0; i<=length; i++){
     Serial.print(payload[i]);
@@ -60,11 +62,14 @@ void capNhatLed(const char * payload, size_t length) {
   Serial.println();
   if(payload[1] == 'N') {
     ledStatus = ON;
+    Serial.println("ONNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNn");
+    Serial.println(ledStatus);
   } else {
     ledStatus = OFF;
+    Serial.println("OFFFFFFFFFFFFFFFFFFFFFFFFFf");
+    Serial.println(ledStatus);
   }
 }
-                               
 /*=======================================Setup===================================================*/
 void setup() {
   
@@ -75,14 +80,17 @@ void setup() {
     ESP.wdtEnable(0);
     pinMode(D5, OUTPUT);
     pinMode(D6, OUTPUT);
-    
+   // pinMode(Button_yellow,INPUT);
+   // pinMode(Button_green,INPUT);
     Val_eeprom = EEPROM.read(0);
     Serial.print("EEPROM_SETUP: ");
     Serial.println(EEPROM.read(0));
     Serial.print("Val_eeprom: ");
     Serial.println(Val_eeprom);
-    digitalWrite(D5,!Val_eeprom);
-    digitalWrite(D6,!Val_eeprom);
+    ledStatus = Val_eeprom;
+    
+    digitalWrite(D5,Val_eeprom);
+    digitalWrite(D6,Val_eeprom);
     WiFi.begin(ssid, pwdWifi);
 
     while (WiFi.status() != WL_CONNECTED)
@@ -90,51 +98,58 @@ void setup() {
       delay(500);
       Serial.print(".");
     }
-  
+
     Serial.println();
     Serial.print("Connected, IP address: ");
     Serial.println(WiFi.localIP());
     
-    webSocket.on("server-send-join-success", joinSuccess);
-    webSocket.on("server-send-togle", togleLed);
-    webSocket.on("yeu-cau-cap-nhat-trang-thai", yeuCauCapNhat);
-    webSocket.on("cap-nhat-led", capNhatLed);
-    webSocket.begin(Host_Socket, Port_Socket, "/socket.io/?transport=websocket");
-    webSocket.emit("esp-send-join-room");
+    //webSocket.on("server-send-join-success", joinSuccess);
+   // webSocket.on("server-bat-tat-led", togleLed);
+   // webSocket.on("server-yeu-cau-cap-nhat-trang-thai-led", Server_capnhatled);
+    //webSocket.on("cap-nhat-led", capNhatLed);
     
-    digitalWrite(D5,!Val_eeprom);
-    digitalWrite(D6,!Val_eeprom);
+  
+    webSocket.on("Req-update-led-from-web", Web_update_led);
+    webSocket.on("Turn-on-off-led-form-web", Turn_on_off_led);
+    
+    webSocket.begin(Host_Socket, Port_Socket, "/socket.io/?transport=websocket");
+    
+    webSocket.emit("esp-join-room");
+    
+    digitalWrite(D5,Val_eeprom);
+    digitalWrite(D6,Val_eeprom);
     
 }
                        
 /*============================Loop==============================================================*/
 void loop() {
-
     if (webSocket.StatusConnectSocket==false)       /* nếu bị mất kết nối với socket thì reset lại */
     {
-      Serial.println("123456789NHU");
-      while(1);
+      ESP.restart();
     }
     
-    if (Val_eeprom != ledStatus && temp == 2)
+    if (Val_eeprom != ledStatus)
     {
-      digitalWrite(D5,!ledStatus);
-      digitalWrite(D6,!ledStatus);
+     
+      digitalWrite(D5,ledStatus);
+      digitalWrite(D6,ledStatus);
       Val_eeprom = ledStatus;
       EEPROM.write(0,Val_eeprom);
       EEPROM.commit();
-      Serial.print("LedStatus: ");
-      Serial.println(ledStatus);
-      Serial.print("Val_eeprom ");
-      Serial.println(Val_eeprom);
-      Serial.print("EEPROM: ");
+      Serial.print("EEPROM_SETUP: ");
       Serial.println(EEPROM.read(0));
+      Serial.print("Val_eeprom: ");
+      Serial.println(Val_eeprom);
+      Serial.print("ledStatus: ");
+      Serial.println(ledStatus);
     }
-    if (temp == 1)
-    {
-      temp = 2;
-      ledStatus = Val_eeprom;
-    }
-    
+  //  if (!digitalRead(Button_yellow))
+  //  {
+  //    while (!digitalRead(Button_yellow));
+  //    if (!digitalRead(D5))
+   //   {
+    //    
+   //   }
+  //  }
     webSocket.loop();
 }
